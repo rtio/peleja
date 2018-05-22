4aedd745e7e859d88820aa13b48a0930673e1555
Rafael Meneses <rafael@rafaelmeneses.me>
2018-05-22 00:12:09 -0300

    config files and bundles

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,6 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 ];
