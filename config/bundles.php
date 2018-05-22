93ae3c87b3aaf500afc75ecd187a0bdb909958b7
Rafael Meneses <rafael@rafaelmeneses.me>
2018-05-22 00:51:42 -0300

    removed debug pack

--- config/bundles.php
+++ config/bundles.php
@@ -5,6 +5,4 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 ];
